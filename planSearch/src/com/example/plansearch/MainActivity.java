68a17a4b718ee423f111dc506ed0a9fe179ce0e7
ter9ng <ttnoren@gmail.com>
2013-02-20 20:25:57 +0100

    UI shhit

--- planSearch/src/com/example/plansearch/MainActivity.java
+++ planSearch/src/com/example/plansearch/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.plansearch;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,22 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
     }
+	
+	public void createClick(View view)
+	{
+		System.out.println("create click");
+		Intent i = new Intent(this,Create.class); 
+		startActivity(i); 
+	}
+	
+	public void joinClick(View view)
+	{
+		System.out.println("join click");
+		Intent i = new Intent(this,Join.class); 
+		startActivity(i); 
+	}
 
 }
