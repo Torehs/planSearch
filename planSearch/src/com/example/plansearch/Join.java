dfd81b2debe6a941abdea4bc594df54a38789bbb
ter9ng <ttnoren@gmail.com>
2013-02-27 10:57:40 +0100

    gui greeir

---
+++ planSearch/src/com/example/plansearch/Join.java
@@ -0,0 +1,16 @@
+package com.example.plansearch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Join extends Activity{
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) 
+	{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.join_main);
+
+	}
+
+}
