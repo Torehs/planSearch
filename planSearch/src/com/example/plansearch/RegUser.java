0b2a15c4293e54f5357674fde831feff3e0a2899
ter9ng <ttnoren@gmail.com>
2013-03-06 12:21:49 +0100

    nye filer

---
+++ planSearch/src/com/example/plansearch/RegUser.java
@@ -0,0 +1,15 @@
+package com.example.plansearch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegUser extends Activity {
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) 
+	{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reg_user_main);
+
+	}
+}
