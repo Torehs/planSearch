22d9f4b30459a5d9aaaaa9c14ff43165de39e2f6
Torehs <torehs@gmail.com>
2013-03-21 17:15:07 +0100

    Transmit-klasse som lagrer informasjon og kommuniserer med server. De fleste metodene er laget og returnerer data, men selve kommunikasjonen er ikke helt klar enda. Men metodene kan implementeres og testes. Fiks gjerne hvis du ser noe som mangler.

--- planSearch/src/com/example/plansearch/Transmit.java
+++ planSearch/src/com/example/plansearch/Transmit.java
@@ -16,46 +16,208 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.example.plansearch.Transmit.Position;
+
 public class Transmit {
 	
-	public int createOperation(String operationName, String operationPassword) {
-		// Build JSONObject Transmit
-		JSONObject jsonT = new JSONObject();
-		try {
-			jsonT.put("operationName", operationName);
-			jsonT.put("operationPassword", operationPassword);
+	// Stored data
+	// Operation
+	static int operationID;
+	static String operationName;
+	static String operationPassword;
+	static String operationDescription;
+	static String operationStartingPoint;
+	static String operationMissingPerson;
+	static String operationLastSeen;
+	
+	// User
+	static int userID;
+	static String userName;
+	static String userPassword;
+	static String userPhone;
+	static String userOrganization;
+	static int userRole; // 0 = no team, 1 = team member, 2 = administrator 
+	static int userTeam;
+	
+	// Logs
+	static int lastLogID;
+	static class Position {
+		int ID;
+		double latitude;
+		double longitude;
+		int userID;
+		boolean searched;
+		
+		public Position(int ID, double latitude, double longitude, int userID, boolean searched) {
+			this.ID = ID;
+			this.latitude = latitude;
+			this.longitude = longitude;
+			this.userID = userID;
+			this.searched = searched;
+		}
+	}
+	static ArrayList<Position> positions = new ArrayList<Position>();
+	
+	// Events
+	static int lastEventID;
+	static ArrayList<String> events = new ArrayList<String>();
+	
+	static public boolean createOperation(String operationName, String operationPassword) {
+		boolean result = false;
+		boolean transmitted = true;
+		
+		if (transmitted) {
+			Transmit.operationID = 15;
+			Transmit.operationName = operationName;
+			Transmit.operationPassword = operationPassword;
+			Transmit.userRole = 2;
 			
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			result = true;
 		}
 		
-		// Build JSONObject Receive
-		JSONObject jsonR = new JSONObject();
+		// FUNGERE IKKE ENDA, MÅ LEGGE INN ASYNC()
+//		// Build JSONObject Transmit
+//		JSONObject jsonT = new JSONObject();
+//		try {
+//			jsonT.put("operationName", operationName);
+//			jsonT.put("operationPassword", operationPassword);
+//			
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		// Build JSONObject Receive
+//		JSONObject jsonR = new JSONObject();
+//		
+//		// Transmit and Receive
+//		try {
+//			jsonR = transmitAndReceive(jsonT);
+//		} catch (ClientProtocolException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		// Decode JSONObject
+//		try {
+//			Transmit.operationID = jsonR.getInt("operationID");
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+
+		return result;
 		
-		try {
-			jsonR = transmitAndReceive(jsonT);
-		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		// Decode JSONObject
-		int operationID = 0;
-		try {
-			operationID = jsonR.getInt("operationID");
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return operationID;
 	}
 	
-	public JSONObject transmitAndReceive(JSONObject jsonT) throws ClientProtocolException, IOException {
+	static public boolean createUser(String userName, String userPassword, String userPhone, String userOrganization) {
+		boolean result = false;
+		boolean transmitted = true;
+		
+		if (transmitted) {
+			userID = 12;
+			Transmit.userName = userName;
+			Transmit.userPassword = userPassword;
+			Transmit.userPhone = userPhone;
+			Transmit.userOrganization = userOrganization;
+			Transmit.userRole = 0;
+			Transmit.userTeam = 0;
+			
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	static public boolean joinOperation(int operationID, String operationPassword) {
+		boolean result = false;
+		boolean transmitted = true;
+		
+		if (transmitted) {
+			Transmit.operationID = 42;
+			Transmit.operationName = "Leteaksjon Vassfjellet";
+			Transmit.operationPassword = "passord";
+			Transmit.operationDescription = "Leteaksjon etter dement mann i Vassfjell-området.";
+			Transmit.operationStartingPoint = "Oppmøte depot Trondheim Røde Kors";
+			Transmit.operationMissingPerson = "64 år gammel mann fra Melhus. Dårlig kledd, men i god form. Rød anorakk, sort bukse, rød lue og hvit sekk.";
+			Transmit.operationLastSeen = "Vassfjell-kapellet kl. 14:45 onsdag.";
+			
+			result = true;
+		}
+		
+		return result;	
+	}
+	
+	static public boolean login(String userName, String userPassword) {
+		boolean result = false;
+		boolean transmitted = true;
+		
+		if (transmitted) {
+			Transmit.userID = 12;
+			Transmit.userName = "Fornavn Etternavn";
+			Transmit.userPassword = "passord";
+			Transmit.userPhone = "+4712345678";
+			Transmit.userOrganization = "Trondheim Røde Kors hjelpekorps";
+			Transmit.userRole = 0;
+			Transmit.userTeam = 0;
+			
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	static public boolean receiveLogs() {
+		boolean result = false;
+		boolean transmitted = true;
+		
+		if (transmitted) {
+			// Not finished
+			positions.add(new Position(720, 10.425342, 55.1313541, 12, true));
+			positions.add(new Position(721, 10.425353, 55.1313521, 12, true));
+			Transmit.lastLogID = 721;
+			
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	static public boolean transmitLogs() {
+		boolean result = false;
+		boolean transmitted = true;
+		
+		if (transmitted) {
+			
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	static public boolean updateOperation(String operationName, String operationPassword, String operationDescription, String operationStartingPoint, String operationMissingPerson, String operationLastSeen) {
+		boolean result = false;
+		boolean transmitted = true;
+		
+		if (transmitted) {
+			Transmit.operationID = 15;
+			Transmit.operationName = operationName;
+			Transmit.operationPassword = operationPassword;
+			Transmit.operationDescription = operationDescription;
+			Transmit.operationStartingPoint = operationStartingPoint;
+			Transmit.operationMissingPerson = operationMissingPerson;
+			Transmit.operationLastSeen = operationLastSeen;
+			
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	static public JSONObject transmitAndReceive(JSONObject jsonT) throws ClientProtocolException, IOException {
 		String myUri = "http://folk.ntnu.no/torehavs/transfer.php";
 		HttpClient httpClient = new DefaultHttpClient();
 		HttpPost httppost = new HttpPost(myUri);
